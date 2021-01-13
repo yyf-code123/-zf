d7ef13f27d4e2a48b1550d567193a9d4019060de
yyf_glb <abc@yyf.com>
2021-01-13 20:29:46 +0800

    update ArticleDao

--- backstage-provider-8001/src/main/java/com/zf/dao/ArticleDao.java
+++ backstage-provider-8001/src/main/java/com/zf/dao/ArticleDao.java
@@ -3,9 +3,17 @@ package com.zf.dao;
 import com.zf.entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface ArticleDao extends JpaRepository<Article,Integer>, JpaSpecificationExecutor<Article> {
-
+    @Transactional
+    @Rollback(false)
+    @Modifying
+    @Query(value = "delete from article where id =?1",nativeQuery = true)
+    public Integer deleteArtilce(Integer id);
 
 }
